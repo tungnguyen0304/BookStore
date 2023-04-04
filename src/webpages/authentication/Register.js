43f2d42aa3dc33be4092e31221085728b6996c1f
vsl5A <114474488+vsl5A@users.noreply.github.com>
2023-04-04 11:05:59 +0700

    Update Register.js

--- src/webpages/authentication/Register.js
+++ src/webpages/authentication/Register.js
@@ -10,18 +10,21 @@ const Register = () => {
   const [selectedDate, setSelectedDate] = useState(null);
   const [Profile, setProfile] = useState({
     name: '',
+    username: '',
     password: '',
-    confirmPassword:'',
+    phone:'',
     email: '',
-    username: ''
+    Address:''
+    
     
   });
   const [errors, setErrors] = useState({
-    name: '',
+     name: '',
+    username: '',
     password: '',
-    confirmPassword:'',
+    phone:'',
     email: '',
-    username: ''
+    Address:''
         
   });    
   
@@ -42,13 +45,24 @@ const Register = () => {
   }
   function checkValidEmail(email) {
     // Regular Expression kiểm tra tính hợp lệ của địa chỉ email
-    const validEmailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    const validEmailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{,50}$/;
   
     // Kiểm tra địa chỉ email có đúng định dạng không
     if (validEmailRegex.test(email)) {
       return true; // Địa chỉ email hợp lệ
     } else {
       return false; // Địa chỉ email không hợp lệ
+    }
+  }
+  function checkValidPass(Pass) {
+    
+    const validPassRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
+  
+    
+    if (validPassRegex.test(Pass)) {
+      return true; 
+    } else {
+      return false; 
     }
   }
   
@@ -60,23 +74,34 @@ const Register = () => {
           errors[name] = ''
       }
   };   
-  // function handleDateSelect(date) {
-  //   console.log(date);
-  //   return date;
-  // }
+  function isValidInput(input) {
+    var pattern = /^[^\d\W_]+$/g; 
+    if (pattern.test(input)) {
+        return true; // Đúng định dạng
+    } else {
+        return false; // Sai định dạng
+    }
+}
+
 
   const handleSubmit = async (e) => {
     // check thong tin truoc khi submit
     const errors = {};
     if (!Profile.name) errors.name = "Tên không được để trống";
+    else if (Profile.name.length >50 ) errors.name = "Tên yêu cầu nhỏ hơn 50 ký tự";
+     else if(!isValidInput(Profile.name)) errors.name = "Tên chỉ bao gồm các ký tự Việt Nam";
     if (!Profile.username) errors.username = "Tài khoản không được để trống";
-    
+    else if ((Profile.username.length < 3) || (Profile.username.length > 20))  errors.username = "username phải từ 3 kí tự đến 20 kí tự";
     if (!checkValidEmail(Profile.email)) {
       errors.email = "Địa chỉ email không hợp lệ";
     } 
-    if ((Profile.password.length < 6) || (Profile.password.length > 15))  errors.password = "Mật khẩu phải từ 6 kí tự đến 15 kí tự";
-    if (Profile.confirmPassword != Profile.password) errors.confirmPassword = "Xác nhận mật khẩu thất bại";
     
+    if (!checkValidPass(Profile.password)) {
+      errors.password = " Password từ 8 ký tự, ít nhất 1 chữ hoa, 1 thường, 1 số, 1 ký tự đặc biệt";
+    } 
+    
+    if((Profile.phone.length != 10)&&(Profile.phone.length != 11)) errors.phone = "Phone từ  10-11 ký tự số";
+    if(Profile.Address.length > 255) errors.Address = "Địa chỉ tối đa 255 ký tự"
     
    
 
@@ -91,9 +116,14 @@ const Register = () => {
     // } catch (error) {
     //   console.log(error);
     // }
-        console.log(123)
-        
-    }      
+    
+    console.log(Profile.name.trim())
+    console.log(Profile.username.trim())
+    console.log(Profile.password.trim())
+    console.log(Profile.phone.trim())
+    console.log(Profile.email.trim())
+    console.log(Profile.Address.trim())
+}      
   };
   const [confirmSaving, setConfirmSaving] = useState(false)
   const [confirmGoingBack, setConfirmGoingBack] = useState(false)
@@ -166,15 +196,29 @@ const Register = () => {
           </Grid>
           <Grid item xs={12} sm={6}>
               <FormControl fullWidth >
-              <label htmlFor="dob">Nhập lại Mật khẩu:</label>
+              <label htmlFor="dob">Address:</label>
               <TextField
-                  type="password"
-                  name="confirmPassword"
+                  type="text"
+                  name="Address"
                   
-                  value={Profile.confirmPassword}
+                  value={Profile.Address}
                   onChange={handleChange}
-                  error={errors.confirmPassword ? true : false}
-                  helperText={errors.confirmPassword}
+                  error={errors.Address ? true : false}
+                  helperText={errors.Address}
+              />
+              </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+              <FormControl fullWidth >
+              <label htmlFor="dob">Phone:</label>
+              <TextField
+                  type="tel"
+                  name="phone"
+                  
+                  value={Profile.phone}
+                  onChange={handleChange}
+                  error={errors.phone ? true : false}
+                  helperText={errors.phone}
               />
               </FormControl>
           </Grid>
